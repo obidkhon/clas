04748541583f01f6925ea8fb552d6ace9600b1d9
obidkon <obid.sharipov.80@mail.ru>
2020-11-07 16:46:33 +0500

    relax

---
+++ src/Cloasscom.java
@@ -0,0 +1,17 @@
+public class Cloasscom {
+    public static void main(String[] args) {
+
+
+                System.out.println("A,B , C 123");
+                System.out.print("!@$");
+                System.out.print("Bu \"Baliq\" ajoyb");
+                System.out.println("bu yreda Qo'shtirnoq bor \"\"" );
+                System.out.println("bu yerda orqa chiziq bor \\");
+                System.out.println("bu \n bola \n ajoyib\n boloa");
+
+
+
+            }
+        }
+
+
